591318bfedbaeff2c8fb5e88afd2528df697f3d6
iheanyi11 <ejeagbaakuegbo@gmail.com>
2024-10-10 00:10:16 +0100

    tried adding a modal

--- scripts/course-page.js
+++ scripts/course-page.js
@@ -118,3 +118,25 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("filter-cse").addEventListener("click", () => filterCourses('CSE'));
     document.getElementById("filter-wdd").addEventListener("click", () => filterCourses('WDD'));
 });
+
+function displayCourseDetails(course) {
+    courseDetails.innerHTML = '';
+    courseDetails.innerHTML = `
+    <button id="closeModal">❌</button>
+    <h2>${course.subject} ${course.number}</h2>
+    <h3>${course.title}</h3>
+    <p><strong>Credits</strong>: ${course.credits}</p>
+    <p><strong>Certificate</strong>: ${course.certificate}</p>
+    <p>${course.description}</p>
+    <p><strong>Technologies</strong>: ${course.technology.join(', ')}</p>
+  `;
+    courseDetails.showModal();
+
+    closeModal.addEventListener("click", () => {
+        courseDetails.close();
+    });
+}
+
+courseDiv.addEventListener('click', () => {
+    displayCourseDetails(course);
+});
